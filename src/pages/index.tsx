abaab6d6eaa1153db822e320c43c8992e1589cb7
Jian Ding <jian.ding@thoughtworks.com>
2023-12-20 13:25:00 +0800

    Update component imports and layout in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { VStack, HStack } from '@chakra-ui/react';
 import BuildStatusOverview from '@/components/BuildStatusOverview';
-import TicketStatusOverview from '@/components/TicketStatusOverview';
-import ProjectTimeline from '@/components/ProjectTimeline';
 import CollapseNavbar from '@/components/CollapseNavbar';
 import UpdateChecker from '@/components/UpdateChecker';
-import OwnerRotationOverview from '@/components/OwnerRotationOverview';
 import AlarmToggleProvider from './AlarmToggleContext';
+import DatadogMonitorOverview from '@/components/DatadogMonitorOverview';
+import DatadogAlertsOverview from '@/components/DatadogAlertsOverview';
 
 export default function Home() {
   return (
@@ -15,13 +14,13 @@ export default function Home() {
         <CollapseNavbar />
         <VStack px="8px" flex="1" width="100vw" overflow="hidden">
           <HStack w="100%" h="100%">
-            <OwnerRotationOverview width="240px" />
+            <DatadogMonitorOverview width="240px" />
             <VStack flex="1" h="100%" overflow="hidden">
-              <HStack h="448px" w="100%">
+              <DatadogAlertsOverview />
+              <HStack h="950px" w="100%">
                 <BuildStatusOverview flex="75%" h="100%" />
-                <TicketStatusOverview flex="25%" h="100%" />
+                {/* <TicketStatusOverview flex="25%" h="100%" /> */}
               </HStack>
-              <ProjectTimeline flex="1" />
             </VStack>
           </HStack>
         </VStack>

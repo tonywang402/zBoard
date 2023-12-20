dc07e65a1b3c0ca64f5c3f175c40fb2e9e4af9be
Jian Ding <jian.ding@thoughtworks.com>
2023-12-20 16:44:13 +0800

    Remove commented out code and add ChristmasImage component

---
+++ src/components/ChristmasImage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Image } from '@chakra-ui/react';
+
+const ChristmasImage = () => {
+  return (
+    <Image
+      src="https://www.gifs.cc/christmas/2022-merry-christmas-animated-puppies.gif"
+      alt="Dan Abramov"
+    />
+  );
+};
+
+export default ChristmasImage;

f89349ca8479608d6cf8993e737c1b5937fd366f
Jian Ding <jian.ding@thoughtworks.com>
2023-12-21 08:08:13 +0800

    Refactor AcknowledgeBox component and add intervalMin and alarmSrc props

--- src/components/AcknowledgeBox.tsx
+++ src/components/AcknowledgeBox.tsx
@@ -1,46 +1,39 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  Tag,
-  TagLabel,
-  TagRightIcon,
-} from '@chakra-ui/react';
+import { Button, Tag, TagLabel, TagRightIcon } from '@chakra-ui/react';
 import { LinkIcon } from '@chakra-ui/icons';
 import { MdBuild } from 'react-icons/md';
-import AlarmContainer from './Alarm/AlarmContainer';
+import AlarmContainer, { AlarmContainerProps } from './Alarm/AlarmContainer';
 import { useAlarmToggle } from '../pages/AlarmToggleContext';
 
-const AcknowledgeBox = () => {
+const AcknowledgeBox: React.FC<AlarmContainerProps> = ({ intervalMin, alarmSrc }) => {
   const [isACKED, setIsACKED] = useState(false);
   const { alarmToggle } = useAlarmToggle();
-  console.log("alarmToggle", alarmToggle);
+  console.log('alarmToggle', alarmToggle);
 
   if (isACKED) {
     return (
-        <Tag
-          size={'lg'}
-          key={'lg'}
-          variant='solid'
-          colorScheme='green'
-        >
-          <TagLabel>ACKED</TagLabel>
-          <TagRightIcon as={MdBuild} />
-        </Tag>
-
+      <Tag size={'lg'} key={'lg'} variant="solid" colorScheme="green">
+        <TagLabel>ACKED</TagLabel>
+        <TagRightIcon as={MdBuild} />
+      </Tag>
     );
   }
 
   return (
     <>
-      <Button leftIcon={<LinkIcon />} colorScheme='teal' variant='solid' onClick={() => {
-        setIsACKED(true);
-      }}>
+      <Button
+        leftIcon={<LinkIcon />}
+        colorScheme="teal"
+        variant="solid"
+        onClick={() => {
+          setIsACKED(true);
+        }}
+      >
         Need ACK
       </Button>
-      {alarmToggle && <AlarmContainer />}
+      {alarmToggle && <AlarmContainer intervalMin={intervalMin} alarmSrc={alarmSrc} />}
     </>
   );
-
 };
 
 export default AcknowledgeBox;
--- src/components/BuildStatusCard.tsx
+++ src/components/BuildStatusCard.tsx
@@ -70,39 +70,41 @@ const BuildStatusCard = ({ buildStatus }: BuildStatusCardProps) => {
   const colorScheme = statusColorScheme[buildStatus.status] || 'red';
   const startTime = moment(buildStatus.stopTime).format('YYYY-MM-DD HH:mm:ss');
   return (
-    <Card color='white' bgColor={`${colorScheme}.500`} p='8px' borderWidth='1px' borderRadius='lg'>
+    <Card color="white" bgColor={`${colorScheme}.500`} p="8px" borderWidth="1px" borderRadius="lg">
       <CardHeader>
         <Flex>
-          <Heading size='xl'>{buildStatus.projectName}</Heading>
+          <Heading size="xl">{buildStatus.projectName}</Heading>
         </Flex>
       </CardHeader>
       <CardBody>
         <HStack>
-          <Badge variant='subtle' colorScheme={colorScheme}>
+          <Badge variant="subtle" colorScheme={colorScheme}>
             {buildStatus.status}
           </Badge>
-          <Flex align='center'>
-            <TimeIcon w='12px' h='12px' />
-            <Text ml='10px'>{startTime}</Text>
+          <Flex align="center">
+            <TimeIcon w="12px" h="12px" />
+            <Text ml="10px">{startTime}</Text>
           </Flex>
         </HStack>
-        <Divider mt='2px' mb='2px' />
-        <Flex align='center'>
-          <Avatar w='32px' h='32px' name={buildStatus.username} src={buildStatus.avatarUrl} />
-          <VStack align='stretch'>
-            <Box ml='4px'>
-              <Text fontSize='md'>
+        <Divider mt="2px" mb="2px" />
+        <Flex align="center">
+          <Avatar w="32px" h="32px" name={buildStatus.username} src={buildStatus.avatarUrl} />
+          <VStack align="stretch">
+            <Box ml="4px">
+              <Text fontSize="md">
                 {buildStatus.username} on <b>{buildStatus.branch}</b>
               </Text>
-              <Text fontSize='md' noOfLines={1}>
+              <Text fontSize="md" noOfLines={1}>
                 {buildStatus.commitSubject}
               </Text>
             </Box>
           </VStack>
         </Flex>
       </CardBody>
-      <CardFooter justify='space-around'>
-        {colorScheme === 'red' && <AcknowledgeBox />}
+      <CardFooter justify="space-around">
+        {colorScheme === 'red' && (
+          <AcknowledgeBox intervalMin={30} alarmSrc={['/audio/sxfr.mp3']} />
+        )}
       </CardFooter>
     </Card>
   );

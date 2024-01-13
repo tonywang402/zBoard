1dd75d5d298fea7884393f19a852d952b0bf613d
ding jian <jian.ding@thoughtworks.com>
2024-01-13 16:25:43 +0800

    fix alarm fade and fix size for TV

--- src/components/BuildStatusCard.tsx
+++ src/components/BuildStatusCard.tsx
@@ -70,7 +70,7 @@ const BuildStatusCard = ({ buildStatus }: BuildStatusCardProps) => {
   const colorScheme = statusColorScheme[buildStatus.status] || 'red';
   const startTime = moment(buildStatus.stopTime).format('YYYY-MM-DD HH:mm:ss');
   return (
-    <Card color="white" bgColor={`${colorScheme}.500`} p="8px" borderWidth="1px" borderRadius="lg">
+    <Card color='white' bgColor={`${colorScheme}.500`} p='8px' borderWidth='1px' borderRadius='lg' maxH="400px">
       <CardHeader>
         <Flex>
           <Heading size="xl">{buildStatus.projectName}</Heading>

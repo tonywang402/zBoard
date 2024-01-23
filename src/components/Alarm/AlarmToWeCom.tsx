b8d5b2828121d22bf66cdf3cb6d64e2109b4a91f
Jian Ding <jian.ding@thoughtworks.com>
2024-01-23 13:07:01 +0800

    Refactor PostAlertToWecom component and pass alertName as a prop

--- src/components/Alarm/AlarmToWeCom.tsx
+++ src/components/Alarm/AlarmToWeCom.tsx
@@ -1,18 +1,14 @@
-const data = {
-  msgtype: 'text',
-  text: {
-    content: `Prod Alert`,
-    mentioned_mobile_list: ['@all'],
-  },
-};
-
 const url =
   'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c275c9c6-175e-45f6-955d-eadcd20d9bbf';
 
 import { useEffect } from 'react';
 import { useToast as useChakraToast } from '@chakra-ui/toast';
 
-const PostAlertToWecom = () => {
+interface PostAlertToWecomProps {
+  alertName: string;
+}
+
+const PostAlertToWecom = (alertInfo: PostAlertToWecomProps) => {
   const toast = useChakraToast();
 
   useEffect(() => {
@@ -21,7 +17,20 @@ const PostAlertToWecom = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        msgtype: 'news',
+        news: {
+          articles: [
+            {
+              title: 'Prod Alert',
+              description: `${alertInfo.alertName}`,
+              url: 'https://app.datadoghq.eu/monitors/manage?q=changan%20status%3Aalert%20env%3Aprod&order=desc',
+              picurl:
+                'https://media.licdn.com/dms/image/C560BAQFLwWfI6v1OPA/company-logo_200_200/0/1657564322231/datadog_logo?e=2147483647&v=beta&t=CYhp1DI8UYvbdgEMj8lGkKqtWvO_4Kel_NJlKRzoz8E',
+            },
+          ],
+        },
+      }),
       mode: 'no-cors',
     })
       .then((response) => {
@@ -37,6 +46,21 @@ const PostAlertToWecom = () => {
       .catch((error) => {
         () => console.warn(error);
       });
+    return () => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          msgtype: 'text',
+          text: {
+            content: `${alertInfo.alertName} is closed`,
+          },
+        }),
+        mode: 'no-cors',
+      });
+    };
   }, []);
   return <></>;
 };

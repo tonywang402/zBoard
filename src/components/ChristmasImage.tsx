1654a768e43077cf7362aa263bb6796c6e2dc4ee
Jian Ding <jian.ding@thoughtworks.com>
2024-02-19 12:39:19 +0800

    Update ChristmasImage component with new images

--- src/components/ChristmasImage.tsx
+++ src/components/ChristmasImage.tsx
@@ -7,9 +7,14 @@ const DefaultImage = () => {
     //   src="https://www.gifs.cc/christmas/2022-merry-christmas-animated-puppies.gif"
     //   alt="Dan Abramov"
     // />
+    // <Image
+    //   src="https://media.tenor.com/K6E74DP8JWgAAAAi/happy-sumikko-gurashi.gif"
+    //   alt="happy new year"
+    // />
     <Image
-      src="https://media.tenor.com/K6E74DP8JWgAAAAi/happy-sumikko-gurashi.gif"
-      alt="happy new year"
+      src="https://wimg.588ku.com/gif620/23/11/07/7af3f73bcd0bb281f38b97d6cc3ccfe6.gif"
+      alt="lunar new year 2024"
+      height="68%"
     />
   );
 };

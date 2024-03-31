5c60e30119c08f8e91e0f38f1b267eda7b20b412
Jian Ding <jian.ding@thoughtworks.com>
2024-03-31 11:12:40 +0800

    Update ChristmasImage component with new spring image

--- src/components/ChristmasImage.tsx
+++ src/components/ChristmasImage.tsx
@@ -11,8 +11,19 @@ const DefaultImage = () => {
     //   src="https://media.tenor.com/K6E74DP8JWgAAAAi/happy-sumikko-gurashi.gif"
     //   alt="happy new year"
     // />
+    // <div>
+    //   <Image
+    //     src="https://i.pinimg.com/originals/27/66/ab/2766ab19e30e26a3dcf20273d3f0faa9.gif"
+    //     alt="spring"
+    //     height="85%"
+    //   />
+    // </div>
     <div>
-      <Image src="/earth-day-happy-earth-day.gif" alt="earth-day-happy-earth-day" height="85%" />
+      <Image
+        src="https://i.pinimg.com/originals/82/ed/f2/82edf20ece5ad261b38e18beac2c03c1.gif"
+        alt="spring"
+        height="80%"
+      />
     </div>
   );
 };

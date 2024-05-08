ad4501e173f4861cbe02638ed79fc17d0d707020
Jian <jian.ding@thoughtworks.com>
2024-05-08 23:25:56 +0800

    Update sorting logic in BuildStatusOverview component

--- src/components/BuildStatusOverview.tsx
+++ src/components/BuildStatusOverview.tsx
@@ -6,11 +6,37 @@ import { useErrorToast } from '@/lib/customToast';
 
 const BuildStatusOverview = (props: SystemProps) => {
   const toastError = useErrorToast();
+  const statusPriority: { [key: string]: number } = {
+    failed: 1,
+    failure: 2,
+    startup_failure: 3,
+    running: 4,
+    in_progress: 5,
+    on_hold: 6,
+    queued: 7,
+    pending: 8,
+    requested: 9,
+    canceled: 10,
+    cancelled: 11,
+    unauthorized: 12,
+    action_required: 13,
+    neutral: 14,
+    skipped: 15,
+    stale: 16,
+    timed_out: 17,
+    waiting: 18,
+    success: 19,
+    completed: 20,
+  };
 
   const fetchData = async () => {
     const res = await fetch('/api/build_status');
     if (res.ok) {
-      return await res.json();
+      const data = await res.json();
+      data.sort((a: BuildStatus, b: BuildStatus) => {
+        return statusPriority[a.status] - statusPriority[b.status];
+      });
+      return data;
     } else {
       toastError(await res.text());
       return [];
@@ -23,7 +49,6 @@ const BuildStatusOverview = (props: SystemProps) => {
       title={buildStatusConfig.title || 'Build Status'}
       onRefresh={fetchData}
       refreshIntervalSeconds={buildStatusConfig.refreshIntervalSeconds || 0}
-      remainOldDataOnError={true}
       render={(data: BuildStatus[]) => (
         <>
           <Grid
@@ -32,10 +57,13 @@ const BuildStatusOverview = (props: SystemProps) => {
             width="100%"
             rowGap="18px"
             columnGap="24px"
-            gridTemplateColumns="repeat(auto-fit, minmax(270px, 1fr))"
+            gridTemplateColumns="repeat(auto-fit, minmax(320px, 1fr))"
           >
-            {data.map((item, index) => (
-              <BuildStatusCard key={index} buildStatus={item} />
+            {data.map((item) => (
+              <BuildStatusCard
+                key={`${item.commitSubject} ${item.status} ${item.stopTime}`}
+                buildStatus={item}
+              />
             ))}
           </Grid>
         </>
